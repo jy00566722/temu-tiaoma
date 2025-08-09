0c30233a073a0a97c6b71aed4fda56d8e870025e
pengfeng <jy00566723@gmail.com>
2025-08-09 18:02:56 +0800

    4

--- jsonToExcel/jsonToExcel.cjs
+++ jsonToExcel/jsonToExcel.cjs
@@ -6,6 +6,8 @@ const jsonData = JSON.parse(fs.readFileSync('input.json', 'utf8'));
 
 // 提取所需字段
 const extractedData = jsonData.map(item => ({
+  '店铺ID':item.labelCodeVO.supplierId,
+  'SPU系统': item.labelCodeVO.productId,
   'SKC系统': item.labelCodeVO.productSkcId,
   'SKU系统': item.labelCodeVO.productSkuId,
   '条码编码': item.labelCodeVO.labelCode,
@@ -13,7 +15,9 @@ const extractedData = jsonData.map(item => ({
   'SKU货号': item.labelCodeVO.skuExtCode,
   '中文颜色': item.productSkuSpecList[0]?.specName || '',
   '尺码': item.productSkuSpecList[1]?.specName || '',
-  '英文颜色': item.productSkuSpecI18nMap.en[0]?.specName || ''
+  '英文颜色': item.productSkuSpecI18nMap.en[0]?.specName || '',
+  '缩略图': item.displayImage || '',
+  '类目标签': item.leafCat.catName || '',
 }));
 
 // 创建 Excel 工作簿和工作表
